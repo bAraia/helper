75866d9e76235a9c88d8d8275859e90ca727ae59
bAraia <araia.bereket@gmail.com>
2023-03-14 16:33:51 -0400

    third  app.js commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+import {AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
-      <header className="App-header">
-        
-       
+                
         <h1>lets build this app please </h1>
         <h2>here is one of the updates</h2>
-      </header>
+      <AmplifySignOut/>
     </div>
+    </AmplifyAuthenticator>
   );
 }
 
