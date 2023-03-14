edc074ace8c55bf5324356cb77d5d4e177eed8bf
bAraia <araia.bereket@gmail.com>
2023-03-14 18:54:00 -0400

    6th commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import awsExports from "./aws-exports";
+import { Amplify } from 'aws-amplify';
 
 Amplify.configure(awsExports);
 
