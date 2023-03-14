7f8e3702da4cfd9f811c146f47960586798f1ce6
bAraia <araia.bereket@gmail.com>
2023-03-14 17:09:48 -0400

    initital 4th commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import {Amplify} from 'aws-amplify';
 import awsConfig from './aws-exports';
-import {AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import {Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <AmplifyAuthenticator>
+    <Authenticator> {({signOut, user}) => (
+      <main>
+        <h1> hello {user.username} </h1>
+        <button onClick={signOut} > Sign Out </button>
+      </main>
+    )}
     <div className="App">
                 
         <h1>lets build this app please </h1>
         <h2>here is one of the updates</h2>
-      <AmplifySignOut/>
     </div>
-    </AmplifyAuthenticator>
+    </Authenticator>
   );
 }
 
